c7da1ed9aac3bf08187e17fb857aeac469596258
Ilias Al Fakir <iliasse2017@outlook.com>
2021-11-22 11:05:33 +0100

    "

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,3 @@
 import CSV from "./CSV";
 
-//example
-(async () => {
-    const csv = new CSV('./files/test.csv');
-    console.log(await csv.read(['product_name', 'brand', 'ean', 'sku', 'stock'], {
-        excludeEmpty: true
-    }))
-    /* await csv.write(['name', 'url', 'price', 'date'], [
-        { name: 'hehe', url: "exp.com", date: new Date() },
-        { name: 'lala', url: "damn.net", price: 15 }
-    ], {
-        dynamic: false
-    }) */
-})()
+export default CSV;
